0583454f8631130989180655370c73d67cb15079
allyface <ally.b.kendall@gmail.com>
2017-09-13 00:02:11 -0700

    completes route 1 of part 1

--- part-1/app.js
+++ part-1/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('This is a successful test.')
+const daysOfWeek = {monday: 1, tuesday:2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7}
+
+app.get('/api/days/:day', (req, res) => {
+  daysOfWeek.hasOwnProperty(req.params.day) ?
+    res.send(`<p>request: GET ${req.url}</p>
+      <p>response: ${daysOfWeek[req.params.day]}</p>
+      <p>status: 200</p>`) :
+    res.send(`<p>request: GET ${req.url}</p>
+      <p>response: '${req.params.day}' is not a valid day!</p>
+      <p>status: 400</p>`)
 })
 
 app.listen(3000, () => {
